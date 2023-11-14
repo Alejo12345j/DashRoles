179afb04036e6ec89fa0b00800efd5016db1d938
Marco Santamaria <marcoalejandrosantamaria@gmail.com>
2023-11-14 11:22:16 -0600

    Culminacion del apartado de roles, blog, usuarios y permisos

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Este seeder nos sirve para crear un usuario y un rol para administrador sin tener nada en la base de datos.
+        $usuario = User::create([
+            'name' => 'Marco Serrano', 
+            'email' => 'fertiequiposnh3@gmail.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+        $rol = Role::create(['name' => 'Administrator']);
+
+        $permisos = Permission::pluck('id', 'id')->all();
+
+        $rol->syncPermissions($permisos);
+
+        $usuario->assignRole([$rol->id]);
+    }
+}
